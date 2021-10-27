0a58920c416d117ff8ad3ec1a04a875ce789264b
김용범 <89058117+Kim-yongbeom@users.noreply.github.com>
2021-10-27 09:21:58 +0900

    연습

---
+++ day2_python/mod1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# mod1.py
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+
+
+# In[ ]:
+
+
+
+
---
+++ day2_python/mod2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+PI = 3.141592
+
+class Math:
+    def solv(self,r):
+        return PI * (r ** 2)
+    
+def add(a,b):
+    return a+b
+
